305efb0b03468254d4968121c8d9ce177d6b3b2d
abc <rahul.banerjee@yahoo.co.in>
2021-04-25 23:31:13 +0530

    Added feign/ribbon
    Added feign/ribbon

--- limits-service/src/main/java/com/javatpoint/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/javatpoint/microservices/limitsservice/LimitsConfigurationController.java
@@ -2,8 +2,11 @@ package com.javatpoint.microservices.limitsservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope ;
 import com.javatpoint.microservices.limitsservice.bean.LimitConfiguration;
+
 @RestController
+@RefreshScope
 public class LimitsConfigurationController 
 {
 @Autowired	
