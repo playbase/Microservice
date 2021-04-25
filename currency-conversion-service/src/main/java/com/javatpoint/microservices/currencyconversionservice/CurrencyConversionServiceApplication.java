46fcfa65639becff23767b2b5b3b26d36e94aee0
abc <rahul.banerjee@yahoo.co.in>
2021-04-26 00:53:58 +0530

    Updated Zuul Peoperties
    Updated Zool Peoperties

--- currency-conversion-service/src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.javatpoint.microservices.currencyconversionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableFeignClients("com.javatpoint.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication 
 {
 public static void main(String[] args) 
---
+++ netflix-zuul-api-gateway-server/src/test/java/com/javatpoint/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.javatpoint.microservices.netflixzuulapigatewayserver;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+
+}
